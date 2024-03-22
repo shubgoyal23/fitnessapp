9b88ba4879f19566d91b98415a331948a7fa83c8
shubgoyal23 <shubgoyal1998@gmail.com>
2024-03-22 11:04:56 +0530

    error file updated

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-app.use(express.json({limit: '16kb'}))
-app.use(express.urlencoded({extended: true, limit: '16kb'}))
+const app = express();
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+app.use((err, req, res, next) => {
+   if (err instanceof ApiError) {
+      res.status(err.statusCode).json({
+         success: false,
+         message: err.message,
+         errors: err.errors,
+      });
+   } else {
+      console.error(err);
+      res.status(500).json({
+         success: false,
+         message: err.message,
+      });
+   }
+});
 
-export {app}
+export { app };
